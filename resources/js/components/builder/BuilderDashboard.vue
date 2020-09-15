a9c8a450e733086dcf4b6635ea09383139a8d373
moikzz214 <moikzz214@gmail.com>
2020-09-15 10:43:17 +0400

    Change header title

--- resources/js/components/builder/BuilderDashboard.vue
+++ resources/js/components/builder/BuilderDashboard.vue
@@ -1,7 +1,7 @@
 <template>
   <v-row justify="center" align="center">
     <v-col class="px-5">
-      <div>Gallega Demo</div>
+      <div>Spinner Platform</div>
     </v-col>
   </v-row>
 </template>
